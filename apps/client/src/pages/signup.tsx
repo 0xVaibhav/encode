e788a06497650f1a377eed54840d9907c63bb9ca
vaibhav <vaibhav0240gupta@gmail.com>
2023-09-17 17:12:42 +0530

    COMPLETED

---
+++ apps/client/src/pages/signup.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+
+function TimerButton() {
+  const [isActive, setIsActive] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(24*60*60); // 24 hours in seconds
+  const [clickCount, setClickCount] = useState(0);
+
+  const startTimer = () => {
+    setIsActive(true);
+    setTimeRemaining(24*60*60); // Reset the timer to 24 hours when clicked
+    setClickCount(clickCount + 1);
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isActive && timeRemaining > 0) {
+      interval = setInterval(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      setIsActive(false);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, timeRemaining]);
+
+  useEffect(() => {
+    // Reset the counter to 0 after 48 hours
+    const timer = setTimeout(() => {
+      setClickCount(0);
+    },48 * 60* 60* 1000); // 48 hours in milliseconds
+
+    return () => clearTimeout(timer);
+  }, [clickCount]);
+
+  const formatTime = (seconds: number) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const pad = (value: number) => (value < 10 ? `0${value}` : value);
+
+    return `${pad(hours)}:${pad(minutes)}:${pad(remainingSeconds)}`;
+  };
+
+  return (
+    <div>
+      <button onClick={startTimer} disabled={isActive}>
+        {isActive ? 'Timer Running' : 'Start 24-Hour Timer'}
+      </button>
+      {isActive && (
+        <div>
+          <p>Time Remaining: {formatTime(timeRemaining)}</p>
+        </div>
+      )}
+      <p>Click Count: {clickCount}</p>
+    </div>
+  );
+}
+
+export default TimerButton;
--- packages/ui/components/signup.tsx
+++ packages/ui/components/signup.tsx
@@ -1,8 +1,36 @@
-export function Signup (){
-    return <div>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-        <button>Signup</button>
+import React, { useState } from 'react';
 
+export function OneTimeButton() {
+  const [isActive, setIsActive] = useState<boolean>(true);
+  const [clickCount, setClickCount] = useState<number>(0);
+
+  // Function to handle the button click
+  const handleClick = () => {
+    if (isActive) {
+      // Perform your action here
+
+      // Update the click count
+      setClickCount(clickCount + 1);
+
+      // Disable the button
+      setIsActive(false);
+
+      // Set a timer to enable the button after 24 hours
+      setTimeout(() => {
+        setIsActive(true);
+      }, 24 * 60 * 60 * 1000); // 24 hours
+    }
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        disabled={!isActive}
+      >
+        {isActive ? 'Click Me' : 'Button Deactivated'}
+      </button>
+      <p>Click Count: {clickCount}</p>
     </div>
+  );
 }
